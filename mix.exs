3b049a6a60ea4f8d39a4b62d3361488cd757fca7
Edham Arief / edthix <edthix@gmail.com>
2016-10-12 01:42:56 +0800

    Completed

--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule Issues.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :issues,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :issues,
+      escript: escript_config,
+      version: "0.1.0",
+      name: "Issues",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,8 +32,14 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.9",
-      poison:    "~> 2.2"
+      { :httpoison, "~> 0.9" },
+      { :poison, "~> 2.2" },
+      { :ex_doc, "~> 0.12" },
+      { :earmark, "~> 1.0", override: true }
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
